6a5604267ce0150548b0408badd1d2748399312a
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2024-04-14 21:11:20 -0500

    update state variables in CreateForm

--- scraper-app/app/form/CreateForm.tsx
+++ scraper-app/app/form/CreateForm.tsx
@@ -4,16 +4,14 @@ import { useRouter } from "next/router"
 import { useState } from "react"
 
 export default function CreateForm() {
+    // def search_classes(session = "", subject = "", match_type = "", match_id = "", career = ""):
 
     const [session, setSession] = useState('')
     const [subject, setSubject] = useState('')
-    const [courseNumber, setCourseNumber] = useState('')
-    const [startingTime, setStartingTime] = useState('')
-    const [endingTime, setEndingTime] = useState('')
-    const [dayOfWeek, setDayOfWeek] = useState('')
-    const [instructorLastName, setInstructorLastName] = useState('')
-    const [mode, setMode] = useState('')
-
+    const [matchType, setMatchType] = useState('')
+    const [matchID, setMatchID] = useState('')
+    const [career, setCareer] = useState('')
+    
     return (
         <h1>This is a form</h1>
     )
