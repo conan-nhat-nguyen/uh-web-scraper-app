153a8bc63ff1db268b69fa5a923858e3a5d9e621
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2024-04-14 21:00:39 -0500

    client can communicate with server, initial setup for form

---
+++ app/form/page.tsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm";
+
+export default function UserForm() {
+    return (
+       <main>
+            <h2 className="text-primary text-center">Submit</h2>
+            <CreateForm />
+       </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import Image from "next/image";
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
+
+  const [message, setMessage] = useState<string>("Loading");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((response) => response.json())
+      .then((data) : void => {
+        setMessage(data.message);
+      })
+  }, [])
+
   return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div>{message}</div>
   );
 }
